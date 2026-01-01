d1e9e53a5324593346af8808cb41bc9ee0b71e7a
Fırat Tuna Arslan <84281538+firatmio@users.noreply.github.com>
2026-01-01 17:23:30 +0300

    README.md ve main.py dosyalarına 2026 yılına ait ilk commit mesajı eklendi.

---
+++ main.py
@@ -0,0 +1,17 @@
+text = r"""
+ _   _      _ _         ____   ___ ____   __   _ 
+| | | | ___| | | ___   |___ \ / _ \___ \ / /_ | |
+| |_| |/ _ \ | |/ _ \    __) | | | |__) | '_ \| |
+|  _  |  __/ | | (_) |  / __/| |_| / __/| (_) |_|
+|_| |_|\___|_|_|\___/  |_____|\___/_____|\___/(_)
+
+This is my first commit of 2026!
+
+Github - https://github.com/firatmio
+"""
+
+def display_banner():
+    print(text)
+    
+if __name__ == "__main__":
+    display_banner()
